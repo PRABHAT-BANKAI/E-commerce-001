96b3752d38067fae3922791d428e78d0c756375a
Arshil <arshilgondaliya5@gmail.com>
2025-08-23 15:49:53 +0530

    login page /db.json

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import  productReducer  from './feature/productSlice'
 import userReducer from './feature/userSlice'
 import cartReducer from './feature/cartSlice'
+import loginReducer from './feature/loginSlice'
 
 export const store = configureStore({
   reducer: { 
      productData:productReducer,
       users: userReducer,
        cart: cartReducer,
+       login: loginReducer,
     },
 })
